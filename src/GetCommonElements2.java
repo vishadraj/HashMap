55c615431774dd8144f91096606d166697018eb1
vishadraj <vishad.vashishtha_cs19@gla.ac.in>
2022-09-06 15:02:36 +0530

    Extend version of Get Common Elements

--- src/GetCommonElements2.java
+++ src/GetCommonElements2.java
@@ -4,42 +4,44 @@
  * Date: 06-09-2022
  */
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class GetCommonElements {
+public class GetCommonElements2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 1st Array :- ");
         int n1 = input.nextInt();
-        int [] a1= new int[n1];
-        for(int i=0; i< a1.length; i++){
+        int [] a1 = new int[n1];
+        for(int i =0; i<a1.length; i++){
             a1[i] = input.nextInt();
         }
 
-
+        System.out.println("Enter 2nd Array :- ");
         int n2 = input.nextInt();
-        int [] a2= new int[n2];
-        for(int i=0; i< a2.length; i++){
+        int [] a2 = new int[n2];
+        for(int i=0; i<a2.length;i++){
             a2[i] = input.nextInt();
         }
 
-//        Arrays.sort(a1);
-//        Arrays.sort(a2);
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for(int val : a1){
             if(hashMap.containsKey(val)){
                 int of = hashMap.get(val);
                 int nf = of +1;
                 hashMap.put(val, nf);
-            } else {
-                hashMap.put(val, 1);
+            } else{
+                hashMap.put(val,1);
             }
         }
+
         for(int val: a2){
-            if(hashMap.containsKey(val)){
+            if(hashMap.containsKey(val) &&  hashMap.get(val) >0 ){
                 System.out.println(val);
-                hashMap.remove(val);
+                int of = hashMap.get(val);
+                int nf = of -1;
+                hashMap.put(val , nf);
             }
         }
 
